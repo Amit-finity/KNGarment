e66f2d07823ad277a72478a0809088b5151ff35f
Amit-031296 <amitv.mumbaii@gmail.com>
2019-10-30 14:29:08 +0530

    forms.py file modified for bs modal update button

--- KNGarment_Order_TrackPro_App/forms.py
+++ KNGarment_Order_TrackPro_App/forms.py
@@ -5,69 +5,34 @@ from bootstrap_modal_forms.forms import BSModalForm
 class Update_FabricOrder(BSModalForm):
     class Meta:
         model= Fabric_Order
-        fields= ('process_vendor_name',
-        'process_vendor_location',
-        'process_received_quantity',
-        'process_received_date',
-        'process_delivery_date',
+        fields= ('process_delivery_date',
         'process_bill_number',
         'process_bill_file',
-        'process_payment_status',
-        'fabric_order_sort_number',
-        'fabric_order_quantity')
+        'process_payment_status')
         
 class Update_StichingOrder(BSModalForm):
     class Meta:
         model= Stiching
-        fields= ('process_vendor_name',
-        'process_vendor_location',
-        'process_received_quantity',
-        'process_received_date',
-        'process_delivery_date',
+        fields= ('process_delivery_date',
         'process_bill_number',
         'process_bill_file',
-        'process_payment_status',
-        'stiching_average_one',
-        'stiching_average_two',
-        'stiching_average_three',
-        'stiching_rate_one',
-        'stiching_rate_two',
-        'stiching_rate_three',
-        'stiching_opening_fabric_stock',
-        'stiching_opening_stock_date',
-        'stiching_fabric_bill_date',
-        'stiching_fabric_used',
-        'stiching_fabric_bill_number')
+        'process_payment_status')
 
 class Update_WashingOrder(BSModalForm):
     class Meta:
         model= Washing
-        fields= ('process_vendor_name',
-        'process_vendor_location',
-        'process_received_quantity',
-        'process_received_date',
-        'process_delivery_date',
+        fields= ('process_delivery_date',
         'process_bill_number',
         'process_bill_file',
-        'process_payment_status',
-        'washing_process_name',
-        'washing_rate',
-        'washing_order_date',
-        'washing_delivery_quantity')
+        'process_payment_status')
 
 class Update_FinishingOrder(BSModalForm):
     class Meta:
         model= Finishing
-        fields= ('process_vendor_name',
-        'process_vendor_location',
-        'process_received_quantity',
-        'process_received_date',
-        'process_delivery_date',
+        fields= ('process_delivery_date',
         'process_bill_number',
         'process_bill_file',
-        'process_payment_status',
-        'finishing_rate',
-        'finishing_delivery_quantity')     
+        'process_payment_status')    
         
 
 class Update_DispatchOrder(BSModalForm):
